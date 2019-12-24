c283c7c59c3809043ef484c60e557d8774d7e2ed
Rachel Fish <itsrachelfish@gmail.com>
2019-12-24 03:02:09 -0700

    Basic js to populate notes

--- strings.js
+++ strings.js
@@ -1,3 +1,46 @@
+const notes = [
+    'A',
+    'A♯',
+    'B',
+    'C',
+    'C♯',
+    'D',
+    'D♯',
+    'E',
+    'F',
+    'F♯',
+    'G',
+    'G♯',
+];
+
+// Standard tuning
+// Todo - Make this dynamic
+const strings = [
+    'E',
+    'A',
+    'D',
+    'G',
+    'B',
+    'E',
+];
+
+function generateNotes(startingNote) {
+    let stringElement = $('.string.template').el[0].cloneNode();
+    $(stringElement).append('<span class="note">' + startingNote + '</span>');
+
+    for(let count = 1; count <= 24; count++) {
+        let currentNote = (notes.indexOf(startingNote) + count) % notes.length;
+
+        $(stringElement).append('<span class="note">' + notes[currentNote] + '</span>');
+    }
+
+    $(stringElement).removeClass('template');
+    $('.notes').append(stringElement);
+}
+
 $(document).ready(() => {
-    console.log('hi');
+    strings.forEach((string) => {
+        console.log('generating', string);
+        generateNotes(string);
+    });
 });

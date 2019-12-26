5b370d06e0655d8a173f6ced9729ad40a1b58288
Rachel Fish <itsrachelfish@gmail.com>
2019-12-26 00:09:24 -0700

    Added dots so it looks like a real fret board!

--- strings.js
+++ strings.js
@@ -24,6 +24,39 @@ const strings = [
     'E',
 ];
 
+const dots = [
+    3, 5, 7, 9, 12, 15, 17
+];
+
+function generateDots() {
+    let string = $('.string.template').el[0].cloneNode();
+    let width = 8;
+
+    for(let count = 0; count <= 19; count++) {
+        let note = $('.note.template').el[0].cloneNode();
+        $(note).removeClass('template');
+
+        if(count > 0) {
+            $(note).style({'width': `${width}%`});
+            width -= 6 / 19;
+
+
+            if(dots.indexOf(count) !== -1) {
+                if(count === 12) {
+                    $(note).text('● ●');
+                } else {
+                    $(note).text('●');
+                }
+            }
+        }
+
+        $(string).append(note);
+    }
+
+    $(string).removeClass('template');
+    $('.notes').append(string);
+}
+
 function generateNotes(startingNote) {
     let string = $('.string.template').el[0].cloneNode();
     let width = 8;
@@ -37,7 +70,7 @@ function generateNotes(startingNote) {
         $(note).attr('note', notes[currentNote]);
 
         if(count > 0) {
-            $(note).style({'width': `calc(${width}% - 2px)`});
+            $(note).style({'width': `${width}%`});
             width -= 6 / 19;
         }
 
@@ -49,6 +82,8 @@ function generateNotes(startingNote) {
 }
 
 $(document).ready(() => {
+    generateDots();
+
     strings.forEach((string) => {
         console.log('generating', string);
         generateNotes(string);
@@ -69,9 +104,9 @@ $(document).ready(() => {
         let note = $(this).attr('note');
 
         if($(this).hasClass('active')) {
-            $(`[note=${note}]`).removeClass('active');
+            $(this).removeClass('active');
         } else {
-            $(`[note=${note}]`).addClass('active');
+            $(this).addClass('active');
         }
     });
 });
